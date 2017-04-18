e445d92152061a15e41505f7155479e471c06ca4
Sloppy <caozhuhua@gmail.com>
2017-04-18 23:21:32 +0800

    add encode and decode

--- py/EncryptFile.py
+++ py/EncryptFile.py
@@ -3,19 +3,26 @@ __author__ = 'sloppy'
 from base64 import b64encode
 from base64 import b64decode
 
-def encodeImage():
+def encodeImage(inputFile,outputFile):
     print("start encode")
-    imgPath=r"C:\Projects\OFF\07_SRC\Client\res\ItemIcon\diamond2.png"
-    with open(imgPath,'rb') as ImageFile:
-        data = ImageFile.read()
+    with open(inputFile,'rb') as InputFile:
+        data = InputFile.read()
         mage_64_encode = b64encode(data)
-        image_64_decode =b64decode(mage_64_encode)
-        print mage_64_encode
-        img = open("1.png","wb")
-        img.write(image_64_decode)
-        img.close()
-        ImageFile.close()
+        outputFile = open(outputFile,"wb")
+        outputFile.write(mage_64_encode)
+        outputFile.close()
+        InputFile.close()
     print("end encode")
-
+def decodeImg(inputFile,outputFile):
+    print("start decode")
+    with open(inputFile,'rb') as InputFile:
+        data = InputFile.read()
+        data_file_decode =b64decode(data)
+        d = open(outputFile,"wb")
+        d.write(data_file_decode)
+        d.close()
+        InputFile.close()
+    print("end decode")
 if __name__=="__main__":
-    encodeImage()
+    encodeImage(r"G:\E:\Media\images\1.jpg",r"G:\E:\Media\images\1_e.jpg");
+    decodeImg(r"G:\E:\Media\images\1_e.jpg",r"G:\E:\Media\images\1_d.jpg")
